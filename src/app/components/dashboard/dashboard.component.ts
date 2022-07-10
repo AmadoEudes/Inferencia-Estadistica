75453f81efbc2439484b290fae5b99de141bb264
Emerson BC <emerson.bendezu.27@unsch.edu.pe>
2022-07-09 19:55:45 -0500

    Presentación

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { Product } from '../../api/product';
 import { ProductService } from '../../service/productservice';
 import { Subscription } from 'rxjs';
-import { ConfigService } from '../../service/app.config.service';
 import { AppConfig } from '../../api/appconfig';
- 
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Product} from '../../api/product';
+import { AppMainComponent } from 'src/app/app.main.component';
+
+
 @Component({
     templateUrl: './dashboard.component.html',
+    providers: [ConfirmationService, MessageService]
 })
 export class DashboardComponent implements OnInit {
 
     items: MenuItem[];
-
-    products: Product[];
 
     chartData: any;
 
@@ -23,109 +24,60 @@ export class DashboardComponent implements OnInit {
 
     config: AppConfig;
 
-    constructor(private productService: ProductService, public configService: ConfigService) {}
+    images: any[];
+
+    products: Product[];
+
+    selectedProduct: Product;
+
+
+constructor(private productService: ProductService, private confirmationService: ConfirmationService, private messageService: MessageService, public appMain: AppMainComponent) {}
 
     ngOnInit() {
-        this.config = this.configService.config;
-        this.subscription = this.configService.configUpdate$.subscribe(config => {
-            this.config = config;
-            this.updateChartOptions();
+        this.productService.getProductsSmall().then(products => this.products = products);
+
+        this.images = [];
+        this.images.push({
+            source: 'assets/demo/images/sopranos/sopranos1.jpg',
+            thumbnail: 'assets/demo/images/sopranos/sopranos1_small.jpg', title: 'Sopranos 1'
         });
-        this.productService.getProductsSmall().then(data => this.products = data);
-          
-        this.items = [
-            {label: 'Add New', icon: 'pi pi-fw pi-plus'},
-            {label: 'Remove', icon: 'pi pi-fw pi-minus'}
-        ];
-
-        this.chartData = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    backgroundColor: '#2f4860',
-                    borderColor: '#2f4860',
-                    tension: .4
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [28, 48, 40, 19, 86, 27, 90],
-                    fill: false,
-                    backgroundColor: '#00bb7e',
-                    borderColor: '#00bb7e',
-                    tension: .4
-                }
-            ]
-        };
+        this.images.push({
+            source: 'assets/demo/images/sopranos/sopranos2.jpg',
+            thumbnail: 'assets/demo/images/sopranos/sopranos2_small.jpg', title: 'Sopranos 2'
+        });
+        this.images.push({
+            source: 'assets/demo/images/sopranos/sopranos3.jpg',
+            thumbnail: 'assets/demo/images/sopranos/sopranos3_small.jpg', title: 'Sopranos 3'
+        });
+        this.images.push({
+            source: 'assets/demo/images/sopranos/sopranos4.jpg',
+            thumbnail: 'assets/demo/images/sopranos/sopranos4_small.jpg', title: 'Sopranos 4'
+        });
     }
 
-    updateChartOptions() {
-        if (this.config.dark)
-            this.applyDarkTheme();
-        else
-            this.applyLightTheme();
-
+    confirm1() {
+        this.confirmationService.confirm({
+            key: 'confirm1',
+            message: 'Are you sure to perform this action?'
+        });
     }
 
-    applyDarkTheme() {
-        this.chartOptions = {
-            plugins: {
-                legend: {
-                    labels: {
-                        color: '#ebedef'
-                    }
-                }
+    confirm2(event: Event) {
+        this.confirmationService.confirm({
+            key: 'confirm2',
+            target: event.target,
+            message: 'Are you sure that you want to proceed?',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.messageService.add({severity: 'info', summary: 'Confirmed', detail: 'You have accepted'});
             },
-            scales: {
-                x: {
-                    ticks: {
-                        color: '#ebedef'
-                    },
-                    grid: {
-                        color:  'rgba(160, 167, 181, .3)',
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: '#ebedef'
-                    },
-                    grid: {
-                        color:  'rgba(160, 167, 181, .3)',
-                    }
-                },
+            reject: () => {
+                this.messageService.add({severity: 'error', summary: 'Rejected', detail: 'You have rejected'});
             }
-        };
+        });
     }
 
-    applyLightTheme() {
-            this.chartOptions = {
-            plugins: {
-                legend: {
-                    labels: {
-                        color: '#495057'
-                    }
-                }
-            },
-            scales: {
-                x: {
-                    ticks: {
-                        color: '#495057'
-                    },
-                    grid: {
-                        color:  '#ebedef',
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: '#495057'
-                    },
-                    grid: {
-                        color:  '#ebedef',
-                    }
-                },
-            }
-        };
+    formatCurrency(value) {
+        return value.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
     }
 }
